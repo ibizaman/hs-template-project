080d5fd2bac976171efa23e4c2d9d6b4c523470c
ibzaman <ibizapeanut@gmail.com>
2020-01-22 21:45:40 -0800

    discover tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,1 @@
-import qualified Test.Hspec as T
-
-import qualified Utils as U
-
-main :: IO ()
-main = T.hspec $
-  T.it "should be equal" (U.equal (1 :: Int) 1)
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
